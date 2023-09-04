9ad3ac1457908ec242245357bb9c3089c251cd40
Fatih Menteşe <fatih.mentese@yahoo.com>
2023-09-04 08:04:35 +0300

    Update and rename create-element.js to yeni-html-elementi.js

--- yeni-html-elementi.js
+++ yeni-html-elementi.js
@@ -1,5 +1,5 @@
 let h1 = document.createElement('h1')
-h1.className = 'New Element'
+h1.className = 'Yeni Başlık'
 h1.style.backgroundColor = 'blue'
-h1.textContent = 'Added with JavaScript Element Order : ' + i
+h1.textContent = 'JavaScript ile eklendi. Element sırası : ' + i
 document.body.appendChild(h1)
